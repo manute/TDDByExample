a43e647497cdaa1f097404f2174096f51aeeb189
Manuel Alonso <manuteali@gmail.com>
2011-10-12 21:13:06 +0200

    Delete Stupid Subclasses

--- MoneyExample/src/com/money/coins/Money.java
+++ MoneyExample/src/com/money/coins/Money.java
@@ -1,11 +1,38 @@
 package com.money.coins;
 
-public class Money {
-	protected int amount;
+ class Money {
 
-	@Override
+	protected int amount;
+	protected String currency;
+
+	Money times(int multiplier){
+		return  new Money(amount * multiplier,currency);
+	}
+
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
+
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
+
+	String currency() {
+		return currency;
+	}
+
 	public boolean equals(Object obj) {
 		Money money = (Money) obj;
-		return amount == money.amount;
+		return amount == money.amount && currency().equals(money.currency());
+	}
+	
+	public String toString() {
+		
+		return amount +" "+currency;
 	}
 }
